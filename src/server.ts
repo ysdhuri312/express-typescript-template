3d8f380de65237e36ad9ea85e84074010632e814
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-07-13 18:36:52 +0530

    Handled uncaught exceptions, rejection, singnal terminations

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,51 @@ import { app } from './app';
 
 const port = 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server runnning on PORT ${port}`);
+});
+
+let isShuttingDown = false;
+const shutdown = (signal: string, exitCode: number) => {
+  if (isShuttingDown) return;
+
+  isShuttingDown = true;
+
+  console.log(`${signal} received. Shutting down gracefully.`);
+
+  server.close(async (error) => {
+    if (error) {
+      console.error('Failed to close HTTP server:', error);
+    }
+
+    try {
+      //   await disconnectDB();
+    } catch (error) {
+      console.error('Failed to disconnect database:', error);
+    }
+
+    process.exit(exitCode); // (1)Process terminated because of an error.(0)successful shutdown.
+  });
+};
+
+// handle promise rejections
+process.on('unhandledRejection', (error) => {
+  console.error('Unhandled Rejection:', error);
+  shutdown('unhandledRejection', 1);
+});
+
+// handled synchronous/runtime exception
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  shutdown('uncaughtException', 1);
+});
+
+// External system (os, docker, etc.) asks your application to stop.
+process.on('SIGTERM', () => {
+  shutdown('SIGTERM', 0);
+});
+
+// SIGINT received (Ctrl+C), Gracefully shutdown.
+process.on('SIGINT', () => {
+  shutdown('SIGINT', 0);
 });
