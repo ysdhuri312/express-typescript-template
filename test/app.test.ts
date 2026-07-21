44a5ab4dbb3659c9166d80d5aa55e5be3e59737e
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-07-21 11:55:54 +0530

    Unistall jest and install vitest

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from '@jest/globals';
+import { describe, test, expect } from 'vitest';
 import { app, sum } from '../src/app.js';
 import request from 'supertest';
 
